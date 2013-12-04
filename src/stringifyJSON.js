707b839c56d1772143baaf64a1ecb62e9bc426bc
brianlk <brian.kang2@gmail.com>
2013-12-04 13:20:36 -0800

    stringifyJSON passing all tests(with the exception of weirdObjects)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,55 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+	var stringed="";
+
+	function travAndString(element){
+
+	  if(typeof element === "function"){
+	  	stringed += "function()";
+	  } else if (typeof element === "object"){
+	  	if(element === null){
+	  		stringed += "null";
+	  	} else if (Array.isArray(element)){
+	  		if(element.length===0){
+	  			stringed += "[]";
+	  		} else {
+		  		stringed += "[";
+		  		_.each(element, function(val){
+		  			travAndString(val);
+		  			stringed += ",";
+		  		});
+		  		stringed = stringed.substr(0, stringed.length-1);
+		  		stringed += "]";
+		  	}
+	  	} else {
+	  		if(Object.keys(element).length===0){
+	  			stringed += "{}";
+	  		} else {
+		  		stringed += "{";
+		  		_.each(element, function(val, key){
+		  			travAndString(key);
+		  			stringed += ":";
+		  			travAndString(val);
+		  			stringed += ",";
+		  		});
+		  		stringed = stringed.substr(0, stringed.length-1);
+		  		stringed += "}";
+		  	}
+	  	}
+	  } else if (typeof element === "number") {
+	  	stringed += element;
+	  } else if (typeof element === "boolean") {
+	  	stringed += element.toString();
+	  } else if (typeof element === "string") {
+	  	stringed += "\""+element+"\"";
+	  }
+	}
+	travAndString(obj);
+	return stringed;
 };
+
+//tostring will fail
+//jsonstringify will string no matter what
+//will always come back as a string(objects, arrays, booleans, etc)
+//might need to construct string version (possibly use typeof)
